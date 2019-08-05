263f12e78fe62eb8b1ca2d9ba4836583e826b6ab
carper <chilingg@qq.com>
2019-08-06 01:22:49 +0800

    Complete class of RDebug

--- rdebug.cpp
+++ rdebug.cpp
@@ -2,5 +2,64 @@
 
 RDebug::RDebug()
 {
-
+    if(++count != 1)
+        printf("%c", 'n');
 }
+
+RDebug::~RDebug()
+{
+    if(--count == 0)
+        printf("%c", '\n');
+}
+
+const RDebug &RDebug::operator<<(int value) const
+{
+    printf("%d ", value);
+    fflush(stdout);
+    return *this;
+}
+
+const RDebug &RDebug::operator<<(unsigned value) const
+{
+    printf("%u ", value);
+    fflush(stdout);
+    return *this;
+}
+
+const RDebug &RDebug::operator<<(double value) const
+{
+    printf("%f ", value);
+    fflush(stdout);
+    return *this;
+}
+
+const RDebug &RDebug::operator<<(const char *str) const
+{
+    printf("%s ", str);
+    fflush(stdout);
+    return *this;
+}
+
+const RDebug &RDebug::operator<<(char s) const
+{
+    printf("%c ", s);
+    fflush(stdout);
+    return *this;
+}
+
+const RDebug &RDebug::operator<<(void *p) const
+{
+    printf("%p ", p);
+    fflush(stdout);
+    return *this;
+}
+
+const RDebug &RDebug::operator<<(bool b) const
+{
+    const char *str = b ? "true" : "fales";
+    printf("%s ", str);
+    fflush(stdout);
+    return *this;
+}
+
+int RDebug::count = 0;
--- rdebug.h
+++ rdebug.h
@@ -1,11 +1,37 @@
 #ifndef RDEBUG_H
 #define RDEBUG_H
 
+#include <cstdio>
+#include <string>
 
 class RDebug
 {
 public:
     RDebug();
+    ~RDebug();
+    RDebug(const RDebug&) = delete;
+    RDebug& operator=(const RDebug&) = delete;
+
+    const RDebug& operator<<(int value) const;
+    const RDebug& operator<<(unsigned value) const;
+    const RDebug& operator<<(double value) const;
+    const RDebug& operator<<(const char *str) const;
+    const RDebug& operator<<(char s) const;
+    const RDebug& operator<<(void* p) const;
+    const RDebug& operator<<(bool b) const;
+
+private:
+    static int count;
 };
+
+inline void printErro(const char *error)
+{
+    fprintf(stderr, "%s", error);
+}
+
+inline void printErro(const std::string &str)
+{
+    printErro(str.c_str());
+}
 
 #endif // RDEBUG_H
