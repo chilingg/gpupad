efb6fefeb4b790e7890bcef7d11186f59bed271b
carper <chilingg@qq.com>
2020-01-30 21:00:20 +0800

    添加RImage类

---
+++ include/redopera/RImage.h
@@ -0,0 +1,59 @@
+#ifndef RIMAGE_H
+#define RIMAGE_H
+
+#include "RResource.h"
+#include "RColor.h"
+#include <stb_image.h>
+
+namespace Redopera {
+
+using RData = uint8_t;
+
+class RImage : public RResource
+{
+    friend void swap(RImage &img1, RImage &img2);
+
+public:
+    static const RImage& redoperaIcon();
+
+    RImage();
+    RImage(const std::string &path, const std::string &name = "Image", bool flip = false);
+    RImage(const RData *data, size_t size, const std::string &name = "Image", bool flip = false);
+    RImage(const RData *data, int width, int height, int channel, const std::string &name = "Image");
+    RImage(const RImage &img);
+    RImage(const RImage &&img);
+    RImage& operator=(RImage img);
+    ~RImage() = default;
+    void swap(RImage &img);
+
+    bool isValid() const;
+    int width() const;
+    int height() const;
+    int channel() const;
+    RData* data() const;
+
+    bool load(const std::string path, bool flip = false);
+    bool load(const RData *buf, size_t size, bool flip = false);
+    bool load(const RData *data, int width, int height, int channel);
+    void flipVertical();
+    void flipHorizontal();
+    void freeImage();
+    void full(const RColor &color);
+
+protected:
+    void copyOnWrite();
+
+private:
+    static const RData REDOPERA_ICON[];
+
+    std::shared_ptr<RData> data_;
+    int width_ = 0;
+    int height_ = 0;
+    int channel_ = 0;
+};
+
+} // Redopera
+
+void swap(Redopera::RImage &img1, Redopera::RImage &img2);
+
+#endif // RIMAGE_H
--- src/dependent/implementation.cpp
+++ src/dependent/implementation.cpp
@@ -4,6 +4,8 @@
 //#define MINIMP3_FLOAT_OUTPUT
 #define MINIMP3_ONLY_MP3
 #define MINIMP3_IMPLEMENTATION
-
 #include "minimp3.h"
 #include "minimp3_ex.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
