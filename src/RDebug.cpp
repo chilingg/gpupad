40b6f6e4d59cd68ff1ecc98646ee561cf320ee5c
carper <chilingg@qq.com>
2020-02-06 16:48:53 +0800

    修正矩阵打印方式

--- src/RDebug.cpp
+++ src/RDebug.cpp
@@ -228,15 +228,16 @@ const RDebug &RDebug::operator<<(const glm::vec4 &vec) const
 
 const RDebug &RDebug::operator<<(const glm::mat4 &mat) const
 {
-    //std::cout << std::setw(6);
-    std::cout << "mat:(" << std::setw(6) << mat[0][0] << ',' << std::setw(6) <<  mat[1][0] << ','
-                         << std::setw(6) << mat[2][0] << ',' << std::setw(6) <<  mat[3][0] << ")\n";
-    std::cout << "    (" << std::setw(6) << mat[0][1] << ',' << std::setw(6) <<  mat[1][1] << ','
-                         << std::setw(6) << mat[2][1] << ',' << std::setw(6) <<  mat[3][1] << ")\n";
-    std::cout << "    (" << std::setw(6) << mat[0][2] << ',' << std::setw(6) <<  mat[1][2] << ','
-                         << std::setw(6) << mat[2][2] << ',' << std::setw(6) <<  mat[3][2] << ")\n";
-    std::cout << "    (" << std::setw(6) << mat[0][3] << ',' << std::setw(6) <<  mat[1][3] << ','
-                         << std::setw(6) << mat[2][3] << ',' << std::setw(6) <<  mat[3][3] << ").";
+    //std::cout << std::setw(8);
+    std::cout << std::left;
+    std::cout << "mat:(" << std::setw(8) << mat[0][0] << " , " << std::setw(8) <<  mat[1][0] << " , "
+                         << std::setw(8) << mat[2][0] << " , " << std::setw(8) <<  mat[3][0] << ")\n";
+    std::cout << "    (" << std::setw(8) << mat[0][1] << " , " << std::setw(8) <<  mat[1][1] << " , "
+                         << std::setw(8) << mat[2][1] << " , " << std::setw(8) <<  mat[3][1] << ")\n";
+    std::cout << "    (" << std::setw(8) << mat[0][2] << " , " << std::setw(8) <<  mat[1][2] << " , "
+                         << std::setw(8) << mat[2][2] << " , " << std::setw(8) <<  mat[3][2] << ")\n";
+    std::cout << "    (" << std::setw(8) << mat[0][3] << " , " << std::setw(8) <<  mat[1][3] << " , "
+                         << std::setw(8) << mat[2][3] << " , " << std::setw(8) <<  mat[3][3] << ").";
     return *this;
 }
 
