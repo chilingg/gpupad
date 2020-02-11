9dcf49b8214f54b02807a491b66fe846231cc3fc
carper <chilingg@qq.com>
2020-02-11 21:08:38 +0800

    修正Uniform的传递与Loc的获取

--- include/redopera/rsc/RShaderProgram.h
+++ include/redopera/rsc/RShaderProgram.h
@@ -20,61 +20,61 @@ public:
     public:
         ~Interface();
 
-        void setViewprot(GLuint loc, float left, float right, float bottom, float top, float near = -127.0f, float far = 128.0f) const;
-        void setPerspective(GLuint loc, float left, float right, float bottom, float top, float near, float far) const;
-        void setCameraMove(GLuint loc, float x, float y, float z = 0) const;
+        void setViewprot(GLint loc, float left, float right, float bottom, float top, float near = -127.0f, float far = 128.0f) const;
+        void setPerspective(GLint loc, float left, float right, float bottom, float top, float near, float far) const;
+        void setCameraMove(GLint loc, float x, float y, float z = 0) const;
 
-        void setUniform(GLuint loc, GLfloat v1) const;
-        void setUniform(GLuint loc, GLfloat v1, GLfloat v2) const;
-        void setUniform(GLuint loc, GLfloat v1, GLfloat v2, GLfloat v3) const;
-        void setUniform(GLuint loc, GLfloat v1, GLfloat v2, GLfloat v3, GLfloat v4) const;
+        void setUniform(GLint loc, GLfloat v1) const;
+        void setUniform(GLint loc, GLfloat v1, GLfloat v2) const;
+        void setUniform(GLint loc, GLfloat v1, GLfloat v2, GLfloat v3) const;
+        void setUniform(GLint loc, GLfloat v1, GLfloat v2, GLfloat v3, GLfloat v4) const;
 
-        void setUniform(GLuint loc, glm::vec3 vec) const;
-        void setUniform(GLuint loc, glm::vec4 vec) const;
+        void setUniform(GLint loc, glm::vec3 vec) const;
+        void setUniform(GLint loc, glm::vec4 vec) const;
 
-        void setUniform(GLuint loc, GLint v1) const;
-        void setUniform(GLuint loc, GLint v1, GLint v2) const;
-        void setUniform(GLuint loc, GLint v1, GLint v2, GLint v3) const;
-        void setUniform(GLuint loc, GLint v1, GLint v2, GLint v3, GLint v4) const;
+        void setUniform(GLint loc, GLint v1) const;
+        void setUniform(GLint loc, GLint v1, GLint v2) const;
+        void setUniform(GLint loc, GLint v1, GLint v2, GLint v3) const;
+        void setUniform(GLint loc, GLint v1, GLint v2, GLint v3, GLint v4) const;
 
-        void setUniform(GLuint loc, glm::ivec3 vec) const;
-        void setUniform(GLuint loc, glm::ivec4 vec) const;
+        void setUniform(GLint loc, glm::ivec3 vec) const;
+        void setUniform(GLint loc, glm::ivec4 vec) const;
 
-        void setUniform(GLuint loc, GLuint v1) const;
-        void setUniform(GLuint loc, GLuint v1, GLuint v2) const;
-        void setUniform(GLuint loc, GLuint v1, GLuint v2, GLuint v3) const;
-        void setUniform(GLuint loc, GLuint v1, GLuint v2, GLuint v3, GLuint v4) const;
+        void setUniform(GLint loc, GLuint v1) const;
+        void setUniform(GLint loc, GLuint v1, GLuint v2) const;
+        void setUniform(GLint loc, GLuint v1, GLuint v2, GLuint v3) const;
+        void setUniform(GLint loc, GLuint v1, GLuint v2, GLuint v3, GLuint v4) const;
 
-        void setUniform(GLuint loc, glm::uvec3 vec) const;
-        void setUniform(GLuint loc, glm::uvec4 vec) const;
+        void setUniform(GLint loc, glm::uvec3 vec) const;
+        void setUniform(GLint loc, glm::uvec4 vec) const;
 
-        void setUniform(GLuint loc, GLsizei size, GLfloat *vp, GLsizei count) const;
-        void setUniform(GLuint loc, GLsizei size, GLint *vp, GLsizei count) const;
-        void setUniform(GLuint loc, GLsizei size, GLuint *vp, GLsizei count) const;
+        void setUniform(GLint loc, GLsizei size, GLfloat *vp, GLsizei count) const;
+        void setUniform(GLint loc, GLsizei size, GLint *vp, GLsizei count) const;
+        void setUniform(GLint loc, GLsizei size, GLuint *vp, GLsizei count) const;
 
-        void setUniform(GLuint loc, glm::vec3 *vec, GLsizei count) const;
-        void setUniform(GLuint loc, glm::vec4 *vec, GLsizei count) const;
-        void setUniform(GLuint loc, glm::ivec3 *vec, GLsizei count) const;
-        void setUniform(GLuint loc, glm::ivec4 *vec, GLsizei count) const;
-        void setUniform(GLuint loc, glm::uvec3 *vec, GLsizei count) const;
-        void setUniform(GLuint loc, glm::uvec4 *vec, GLsizei count) const;
+        void setUniform(GLint loc, glm::vec3 *vec, GLsizei count) const;
+        void setUniform(GLint loc, glm::vec4 *vec, GLsizei count) const;
+        void setUniform(GLint loc, glm::ivec3 *vec, GLsizei count) const;
+        void setUniform(GLint loc, glm::ivec4 *vec, GLsizei count) const;
+        void setUniform(GLint loc, glm::uvec3 *vec, GLsizei count) const;
+        void setUniform(GLint loc, glm::uvec4 *vec, GLsizei count) const;
 
-        void setUniformMatrix(GLuint loc, const glm::mat2 &mat) const;
-        void setUniformMatrix(GLuint loc, const glm::mat3 &mat) const;
-        void setUniformMatrix(GLuint loc, const glm::mat4 &mat) const;
-        void setUniformMatrix(GLuint loc, const glm::dmat2 &mat) const;
-        void setUniformMatrix(GLuint loc, const glm::dmat3 &mat) const;
-        void setUniformMatrix(GLuint loc, const glm::dmat4 &mat) const;
+        void setUniformMatrix(GLint loc, const glm::mat2 &mat) const;
+        void setUniformMatrix(GLint loc, const glm::mat3 &mat) const;
+        void setUniformMatrix(GLint loc, const glm::mat4 &mat) const;
+        void setUniformMatrix(GLint loc, const glm::dmat2 &mat) const;
+        void setUniformMatrix(GLint loc, const glm::dmat3 &mat) const;
+        void setUniformMatrix(GLint loc, const glm::dmat4 &mat) const;
 
-        void setUniformMatrix(GLuint loc, const glm::mat2 *mat, GLsizei count) const;
-        void setUniformMatrix(GLuint loc, const glm::mat3 *mat, GLsizei count) const;
-        void setUniformMatrix(GLuint loc, const glm::mat4 *mat, GLsizei count) const;
-        void setUniformMatrix(GLuint loc, const glm::dmat2 *mat, GLsizei count) const;
-        void setUniformMatrix(GLuint loc, const glm::dmat3 *mat, GLsizei count) const;
-        void setUniformMatrix(GLuint loc, const glm::dmat4 *mat, GLsizei count) const;
+        void setUniformMatrix(GLint loc, const glm::mat2 *mat, GLsizei count) const;
+        void setUniformMatrix(GLint loc, const glm::mat3 *mat, GLsizei count) const;
+        void setUniformMatrix(GLint loc, const glm::mat4 *mat, GLsizei count) const;
+        void setUniformMatrix(GLint loc, const glm::dmat2 *mat, GLsizei count) const;
+        void setUniformMatrix(GLint loc, const glm::dmat3 *mat, GLsizei count) const;
+        void setUniformMatrix(GLint loc, const glm::dmat4 *mat, GLsizei count) const;
 
-        void setUniformMatrix(GLuint loc, GLsizei order, GLfloat *vp, GLsizei count = 1, GLboolean transpose = false) const;
-        void setUniformMatrix(GLuint loc, GLsizei order, GLdouble *vp, GLsizei count = 1, GLboolean transpose = false) const;
+        void setUniformMatrix(GLint loc, GLsizei order, GLfloat *vp, GLsizei count = 1, GLboolean transpose = false) const;
+        void setUniformMatrix(GLint loc, GLsizei order, GLdouble *vp, GLsizei count = 1, GLboolean transpose = false) const;
 
     private:
         Interface(GLuint id);
@@ -96,7 +96,7 @@ public:
     GLuint shaderProgramID() const;
     Interface useInterface() const;
 
-    GLuint getUniformLocation(const std::string &name) const;
+    GLint getUniformLocation(const std::string &name) const;
 
     // attachShader() 与 detachShader() 都是只在重新linkProgram时生效
     void attachShader(std::initializer_list<RShader> list);
--- src/RShaderProgram.cpp
+++ src/RShaderProgram.cpp
@@ -16,115 +16,115 @@ RShaderProgram::Interface::~Interface()
     }
 }
 
-void RShaderProgram::Interface::setViewprot(GLuint loc, float left, float right, float bottom, float top, float near, float far) const
+void RShaderProgram::Interface::setViewprot(GLint loc, float left, float right, float bottom, float top, float near, float far) const
 {
     glm::mat4 projection = glm::ortho(left, right, bottom, top, near, far);
     setUniformMatrix(loc, 4, glm::value_ptr(projection));
 }
 
-void RShaderProgram::Interface::setPerspective(GLuint loc, float left, float right, float bottom, float top, float near, float far) const
+void RShaderProgram::Interface::setPerspective(GLint loc, float left, float right, float bottom, float top, float near, float far) const
 {
     glm::mat4 projection = perspective(left, right, bottom, top, near, far);
     setUniformMatrix(loc, 4, glm::value_ptr(projection));
 }
 
-void RShaderProgram::Interface::setCameraMove(GLuint loc, float x, float y, float z) const
+void RShaderProgram::Interface::setCameraMove(GLint loc, float x, float y, float z) const
 {
     glm::mat4 view = glm::translate(glm::mat4(1), {-x, -y, -z});
     setUniformMatrix(loc, 4, glm::value_ptr(view));
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLfloat v1) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLfloat v1) const
 {
     glUniform1f(loc, v1);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLfloat v1, GLfloat v2) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLfloat v1, GLfloat v2) const
 {
     glUniform2f(loc, v1, v2);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLfloat v1, GLfloat v2, GLfloat v3) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLfloat v1, GLfloat v2, GLfloat v3) const
 {
     glUniform3f(loc, v1, v2, v3);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLfloat v1, GLfloat v2, GLfloat v3, GLfloat v4) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLfloat v1, GLfloat v2, GLfloat v3, GLfloat v4) const
 {
     glUniform4f(loc, v1, v2, v3, v4);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::vec3 vec) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::vec3 vec) const
 {
     glUniform3f(loc, vec.x, vec.y, vec.z);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::vec4 vec) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::vec4 vec) const
 {
     glUniform4f(loc, vec.x, vec.y, vec.z, vec.w);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLint v1) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLint v1) const
 {
     glUniform1i(loc, v1);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLint v1, GLint v2) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLint v1, GLint v2) const
 {
     glUniform2i(loc, v1, v2);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLint v1, GLint v2, GLint v3) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLint v1, GLint v2, GLint v3) const
 {
     glUniform3i(loc, v1, v2, v3);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLint v1, GLint v2, GLint v3, GLint v4) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLint v1, GLint v2, GLint v3, GLint v4) const
 {
     glUniform4i(loc, v1, v2, v3, v4);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::ivec3 vec) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::ivec3 vec) const
 {
     glUniform3i(loc, vec.x, vec.y, vec.z);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::ivec4 vec) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::ivec4 vec) const
 {
     glUniform4i(loc, vec.x, vec.y, vec.z, vec.w);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLuint v1) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLuint v1) const
 {
     glUniform1ui(loc, v1);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLuint v1, GLuint v2) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLuint v1, GLuint v2) const
 {
     glUniform2ui(loc, v1, v2);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLuint v1, GLuint v2, GLuint v3) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLuint v1, GLuint v2, GLuint v3) const
 {
     glUniform3ui(loc, v1, v2, v3);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLuint v1, GLuint v2, GLuint v3, GLuint v4) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLuint v1, GLuint v2, GLuint v3, GLuint v4) const
 {
     glUniform4ui(loc, v1, v2, v3, v4);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::uvec3 vec) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::uvec3 vec) const
 {
     glUniform3ui(loc, vec.x, vec.y, vec.z);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::uvec4 vec) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::uvec4 vec) const
 {
     glUniform4ui(loc, vec.x, vec.y, vec.z, vec.w);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLsizei size, GLfloat *vp, GLsizei count) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLsizei size, GLfloat *vp, GLsizei count) const
 {
     switch(size) {
     case 1:
@@ -144,7 +144,7 @@ void RShaderProgram::Interface::setUniform(GLuint loc, GLsizei size, GLfloat *vp
     }
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLsizei size, GLint *vp, GLsizei count) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLsizei size, GLint *vp, GLsizei count) const
 {
     switch(size) {
     case 1:
@@ -165,7 +165,7 @@ void RShaderProgram::Interface::setUniform(GLuint loc, GLsizei size, GLint *vp, 
     }
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, GLsizei size, GLuint *vp, GLsizei count) const
+void RShaderProgram::Interface::setUniform(GLint loc, GLsizei size, GLuint *vp, GLsizei count) const
 {
     switch(size) {
     case 1:
@@ -186,97 +186,97 @@ void RShaderProgram::Interface::setUniform(GLuint loc, GLsizei size, GLuint *vp,
     }
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::vec3 *vec, GLsizei count) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::vec3 *vec, GLsizei count) const
 {
     glUniform3fv(loc, count, &vec->x);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::vec4 *vec, GLsizei count) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::vec4 *vec, GLsizei count) const
 {
     glUniform4fv(loc, count, &vec->x);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::ivec3 *vec, GLsizei count) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::ivec3 *vec, GLsizei count) const
 {
     glUniform3iv(loc, count, &vec->x);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::ivec4 *vec, GLsizei count) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::ivec4 *vec, GLsizei count) const
 {
     glUniform4iv(loc, count, &vec->x);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::uvec3 *vec, GLsizei count) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::uvec3 *vec, GLsizei count) const
 {
     glUniform3uiv(loc, count, &vec->x);
 }
 
-void RShaderProgram::Interface::setUniform(GLuint loc, glm::uvec4 *vec, GLsizei count) const
+void RShaderProgram::Interface::setUniform(GLint loc, glm::uvec4 *vec, GLsizei count) const
 {
     glUniform4uiv(loc, count, &vec->x);
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::mat2 &mat) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::mat2 &mat) const
 {
-    glUniformMatrix2fv(loc, count, GL_FALSE, glm::value_ptr(mat));
+    glUniformMatrix2fv(loc, 1, GL_FALSE, glm::value_ptr(mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::mat3 &mat) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::mat3 &mat) const
 {
-    glUniformMatrix3fv(loc, count, GL_FALSE, glm::value_ptr(mat));
+    glUniformMatrix3fv(loc, 1, GL_FALSE, glm::value_ptr(mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::mat4 &mat) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::mat4 &mat) const
 {
-    glUniformMatrix4fv(loc, count, GL_FALSE, glm::value_ptr(mat));
+    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::dmat2 &mat) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::dmat2 &mat) const
 {
-    glUniformMatrix2dv(loc, count, GL_FALSE, glm::value_ptr(mat));
+    glUniformMatrix2dv(loc, 1, GL_FALSE, glm::value_ptr(mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::dmat3 &mat) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::dmat3 &mat) const
 {
-    glUniformMatrix3dv(loc, count, GL_FALSE, glm::value_ptr(mat));
+    glUniformMatrix3dv(loc, 1, GL_FALSE, glm::value_ptr(mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::dmat4 &mat) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::dmat4 &mat) const
 {
-    glUniformMatrix4dv(loc, count, GL_FALSE, glm::value_ptr(mat));
+    glUniformMatrix4dv(loc, 1, GL_FALSE, glm::value_ptr(mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::mat2 *mat, GLsizei count) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::mat2 *mat, GLsizei count) const
 {
     glUniformMatrix2fv(loc, count, GL_FALSE, glm::value_ptr(*mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::mat3 *mat, GLsizei count) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::mat3 *mat, GLsizei count) const
 {
     glUniformMatrix3fv(loc, count, GL_FALSE, glm::value_ptr(*mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::mat4 *mat, GLsizei count) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::mat4 *mat, GLsizei count) const
 {
     glUniformMatrix4fv(loc, count, GL_FALSE, glm::value_ptr(*mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::dmat2 *mat, GLsizei count) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::dmat2 *mat, GLsizei count) const
 {
     glUniformMatrix2dv(loc, count, GL_FALSE, glm::value_ptr(*mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::dmat3 *mat, GLsizei count) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::dmat3 *mat, GLsizei count) const
 {
     glUniformMatrix3dv(loc, count, GL_FALSE, glm::value_ptr(*mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, const glm::dmat4 *mat, GLsizei count) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, const glm::dmat4 *mat, GLsizei count) const
 {
     glUniformMatrix4dv(loc, count, GL_FALSE, glm::value_ptr(*mat));
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, GLsizei order, GLfloat *vp, GLsizei count, GLboolean transpose) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, GLsizei order, GLfloat *vp, GLsizei count, GLboolean transpose) const
 {
     switch(order) {
     case 2:
@@ -294,7 +294,7 @@ void RShaderProgram::Interface::setUniformMatrix(GLuint loc, GLsizei order, GLfl
     }
 }
 
-void RShaderProgram::Interface::setUniformMatrix(GLuint loc, GLsizei order, GLdouble *vp, GLsizei count, GLboolean transpose) const
+void RShaderProgram::Interface::setUniformMatrix(GLint loc, GLsizei order, GLdouble *vp, GLsizei count, GLboolean transpose) const
 {
     switch(order) {
     case 2:
@@ -383,9 +383,11 @@ RShaderProgram::Interface RShaderProgram::useInterface() const
     return Interface(*progID_);
 }
 
-GLuint RShaderProgram::getUniformLocation(const std::string &name) const
+GLint RShaderProgram::getUniformLocation(const std::string &name) const
 {
-    return glGetUniformLocation(*progID_, name.c_str());
+    GLint loc = glGetUniformLocation(*progID_, name.c_str());
+    check(loc < 0, "Invalid locale <" + name + '>');
+    return loc;
 }
 
 void RShaderProgram::attachShader(std::initializer_list<RShader> list)
